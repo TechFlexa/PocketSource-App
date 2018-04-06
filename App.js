9f139b41a124039246bc0e97a6d41a8e1d2c3ae8
Vinay Khobragade <vinaykhobragade99@gmail.com>
2018-04-06 20:32:13 +0530

    refactor: codebase

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Root } from 'native-base';
-import Router from './source/Router';
+import Router from './src/routes/Router';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Root>
         <Router />
-      </Root>
     );
   }
 }
